2e9b5c038bdfb89c6c5f7754f448dfea42aec5b4
Risha <rishallen@gmail.com>
2016-08-16 19:47:38 -0700

    new app that does cors.

---
+++ models/sentiment.js
@@ -0,0 +1,10 @@
+// // app/models/sentiment.js
+//
+// var mongoose     = require('mongoose');
+// var Schema       = mongoose.Schema;
+//
+// var sentimentSchema   = new Schema({
+//     name: String
+// });
+//
+// module.exports = mongoose.model('sentiment', sentimentSchema);
