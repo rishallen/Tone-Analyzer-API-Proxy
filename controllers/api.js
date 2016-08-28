ff385f10db256f231e5e7090894a13345c34259d
Risha <rishallen@gmail.com>
2016-08-28 12:47:04 -0700

    added elements to my index page.

--- controllers/api.js
+++ controllers/api.js
@@ -1,22 +1,24 @@
 "use strict";
-var keys = require('../keys.js');
+var keys           = require('../keys.js');
 var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
-let request = require('request');
-let username = keys.username;
-let password = keys.password;
+let request        = require('request');
+var pick           = require('object.pick');
+var extend         = require('extend');
+let username       = keys.username;
+let password       = keys.password;
 
 // this is connected to the routes in server.js
-
   module.exports = {
     //the attribute(analysis) holds the function
-    // query = request body from the server.js file
+    //query = request body from the server.js file
     getAnalysis: function(query, callback) {
       var tone_analyzer = new ToneAnalyzerV3({
         username: username,
         password: password,
         version: 'v3',
         version_date: '2016-05-19',
-  });
+        qs: pick(['tones', 'sentences'])
+    });
 
     tone_analyzer.tone({
       // this holds the text
@@ -24,4 +26,3 @@ let password = keys.password;
     }, callback); // takes a function(error, body) this comes from server.js
   }
 };
-// when you require this file, you get the emotion object with the analysis function in it
--- server.js
+++ server.js
@@ -11,11 +11,31 @@ var cors = require('cors');
 var apiController = require('./controllers/api');
 var https = require('https');
 var fs = require('fs');
+var path = require("path");
+var d3 = require('d3');
+var d3 = require("d3"),
+    jsdom = require("jsdom");
+var document = jsdom.jsdom(),
+    svg = d3.select(document.body).append("svg");
+
+'use strict';
+
+// Run some jQuery on a html fragment
+var jsdom = require("jsdom");
+
+jsdom.env(
+  '<p><a class="the-link" href="https://github.com/tmpvar/jsdom">jsdom!</a></p>',
+  ["http://code.jquery.com/jquery.js"],
+  function (err, window) {
+    console.log("contents of a.the-link:", window.$("a.the-link").text());
+  }
+);
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 //Enable All CORS Requests
 app.use(cors());
@@ -27,14 +47,13 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// not being used
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    // res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 // endpoint
 router.post('/v1/analyze', function(req, res) {
-  // console.log('we have arrived!', req.body);
   res.json({
     message: req.body.text
   });
@@ -51,11 +70,11 @@ router.post('/analyze', function (request, response, next) { // function is a ca
   // this is being passed
   // console.log('this is a body request', request.body);
   apiController.getAnalysis(request.body, function (error, apiResponseobject, body) {  // json always patterned as error, results
-    // console.log('this is the body response', body);
+    console.log('this is the body response', body);
     // console.log('this is the error', error);
     if (error) { response.status(500).json({ error: error.message }); }
     // if no error, render the results
-    console.log('reached success!');
+    // console.log('reached success!');
         // console.log('what is in here?', body.body);
     response.json(JSON.parse(body.body));
 
@@ -67,7 +86,7 @@ router.post('/analyze', function (request, response, next) { // function is a ca
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+// all of our routes will be prefixed with /
 app.use('/', router);
 
 // START THE SERVER
